876d1467a75bc5045f1c414111e6c0473bc19001
Thainan Remboski <tbremboski@inf.ufpel.edu.br>
2015-10-23 16:07:35 -0200

    Duas funcoes a mais adicionadas

--- leap_library.py
+++ leap_library.py
@@ -6,8 +6,14 @@ sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 import Leap
 from Leap import Finger
 
+# @param: hand
+# @return: list of distances (thumb -> index)
+#							 (index -> middle)
+#							 (middle -> ring)
+#							 (ring -> pinky)
 def distance_between_fingers(hand):
 	if hand.is_valid:
+	# if True:
 		distance = []
 
 		thumb = hand.fingers[Finger.TYPE_THUMB].tip_position
@@ -31,14 +37,84 @@ def distance_between_fingers(hand):
 
 	return None
 
+# @param: hand
+# @return: list of distances (palm -> thumb) 
+#							 (palm -> index)
+#							 (palm -> middle)
+#							 (palm -> ring)
+#							 (palm -> pinky)
+def distance_to_fingertips(hand):
+	if hand.is_valid:
+	# if True:
+		distance = []
+
+		thumb = hand.fingers[Finger.TYPE_THUMB].tip_position
+		index = hand.fingers[Finger.TYPE_INDEX].tip_position
+		middle = hand.fingers[Finger.TYPE_MIDDLE].tip_position
+		ring = hand.fingers[Finger.TYPE_RING].tip_position
+		pinky = hand.fingers[Finger.TYPE_PINKY].tip_position
+
+		palm = hand.palm_position
+
+		# thumb = Leap.Vector(10, 0, 0)
+		# index = Leap.Vector(0, 20, 0)
+		# middle = Leap.Vector(10, 20, 0)
+		# ring = Leap.Vector(10, 0, 30)
+		# pinky = Leap.Vector(0, 0, 30)
+
+		# palm = Leap.Vector(10, 20, 10)
+
+		distance.append(palm.distance_to(thumb))
+		distance.append(palm.distance_to(index))
+		distance.append(palm.distance_to(middle))
+		distance.append(palm.distance_to(ring))
+		distance.append(palm.distance_to(pinky))
+
+		return distance
+
+	return None
+
+# @param: hand
+# @return: list of Leap.Vector (thumb, index, middle, ring, pinky) 
+def translate_to_origin(hand):
+	if hand.is_valid:
+	# if True:
+		fingers = []
+
+		thumb = hand.fingers[Finger.TYPE_THUMB].tip_position
+		index = hand.fingers[Finger.TYPE_INDEX].tip_position
+		middle = hand.fingers[Finger.TYPE_MIDDLE].tip_position
+		ring = hand.fingers[Finger.TYPE_RING].tip_position
+		pinky = hand.fingers[Finger.TYPE_PINKY].tip_position
+
+		palm = hand.palm_position
+
+		# thumb = Leap.Vector(10, 0, 0)
+		# index = Leap.Vector(0, 20, 0)
+		# middle = Leap.Vector(10, 20, 0)
+		# ring = Leap.Vector(10, 0, 30)
+		# pinky = Leap.Vector(0, 0, 30)
+
+		# palm = Leap.Vector(10, 20, 10)
+
+		fingers.append(thumb - palm)
+		fingers.append(index - palm)
+		fingers.append(middle - palm)
+		fingers.append(ring - palm)
+		fingers.append(pinky - palm)
+
+		return fingers
+
+	return None
+
 
 def main(argv):
 	hand = Leap.Hand()
 
-	d = distance_between_fingers(hand)
+	a = translate_to_origin(hand)
 
-	print d
-
+	# for aa in a:
+	# 	print aa
 
 
 if __name__ == '__main__':
