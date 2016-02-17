2992aef6d308934e4966feb8acd91ab0a5724593
wdalmorra <wddsouza@inf.ufpel.edu.br>
2016-02-16 22:39:03 -0800

    arrumado o csv

--- GesturesRecord/Interface/data/gera_colunas.py
+++ GesturesRecord/Interface/data/gera_colunas.py
@@ -3,27 +3,35 @@ import sys
 import pprint
 
 columns = []
+numbers = []
 
-def name_columns_rec(name, data):
+def name_columns_rec(name, data,index):
 
 	if type(data) is list:
 		columns.append(name+'.'+'X')
+		numbers[index].append(data[0])
 		columns.append(name+'.'+'Y')
+		numbers[index].append(data[1])
 		columns.append(name+'.'+'Z')
+		numbers[index].append(data[2])
 	elif type(data) is not dict:
 		columns.append(name)
+		numbers[index].append(data)
 	else:
 		for n in data:
-			name_columns_rec(name+'.'+n, data[n])
+			name_columns_rec(name+'.'+n, data[n],index)
 
 def name_columns(file):
 	data = []
 	with open(file) as json_file:
 		for line in json_file:
 			data.append(json.loads(line))
+	# print len(data)
 
-	for l in data[0]:
-		name_columns_rec(l,data[0][l])
+	for i in range (len(data)):
+		for l in data[i]:
+			numbers.append([])
+			name_columns_rec(l,data[i][l],i)
 
 
 
@@ -31,8 +39,10 @@ def main(argv):
 	
 	name_columns(argv)
 
-	for i in columns:
-		print i
+	for i in range(80):
+		for j in range(len(numbers[i])):
+			print columns[j] + ": " + str(numbers[i][j])
+			# print numbers[i][j]
 
 if __name__ == '__main__':
 	main(sys.argv[1])
