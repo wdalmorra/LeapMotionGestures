08b3c106136baccc0276ff46c64088e52394b04a
wdalmorra <wddsouza@inf.ufpel.edu.br>
2016-03-14 16:46:25 -0300

    historico adicionado

--- GestureAnalisys/gui.py
+++ GestureAnalisys/gui.py
@@ -7,6 +7,8 @@ from PyQt5.QtCore import *
 
 class Example(QWidget):
 
+	last_guesses = ['1','2','3','4','5','6','7','8','9','10']
+
 	def __init__(self, parent=None):
 		QWidget.__init__(self,parent)
 		self.__initUI()
@@ -20,17 +22,34 @@ class Example(QWidget):
 
 	def __initUI(self):
 
-		self.gesture_label = QLabel("-", self)
+		self.gesture_label = QLabel("3", self)
+		new_font = QFont("Times", 150, QFont.Bold)
+		self.gesture_label.setFont(new_font)
+
+		tmp = ''
+		for g in self.last_guesses:
+			tmp = g + " " + tmp
+		
+		self.history_label = QLabel(tmp,self)
+		new_font2 = QFont("Times", 20, QFont.Bold)
+		self.history_label.setFont(new_font2)
+
+
 
 		hbox = QHBoxLayout()
 		hbox.addStretch(1)
 		hbox.addWidget(self.gesture_label)
 		hbox.addStretch(1)
 
+		hbox2 = QHBoxLayout()
+		hbox2.addStretch(1)
+		hbox2.addWidget(self.history_label)
+
 		vbox = QVBoxLayout()
 		vbox.addStretch(1)
 		vbox.addLayout(hbox)
 		vbox.addStretch(1)
+		vbox.addLayout(hbox2)
 
 		self.setLayout(vbox)
 
@@ -50,6 +69,21 @@ class Example(QWidget):
 	# Updates the message label with the correct message
 	def update_gesture_label(self, gest):
 		self.gesture_label.setText(gest)
+
+		self.last_guesses = self.last_guesses[1:]
+		self.last_guesses.append(gest)
+
+		tmp = ''
+		for g in self.last_guesses:
+			tmp = g + " " + tmp
+		
+		self.history_label = QLabel(tmp,self)
+		new_font = QFont("Times", 30, QFont.Bold)
+		self.gesture_label.setFont(new_font)
+
+
+
+
 
 	def closeEvent(self, event):
 
